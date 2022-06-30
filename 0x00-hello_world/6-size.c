7a2bef40a1aa888cf0b520ae7643cd7f5ec51ae0
Bradleykibs <Bradleykibwana@gmail.com>
2022-06-30 14:51:33 +0000

    spacing problem

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  *
  */
 int main(void)
-{	printf("The size of float is: %d\n byte(s)\n", sizeof(float));
-	printf("The size of char is: %d\n byte(s)\n", sizeof(char));
-	printf("The size of int is: %d\n byte(s)\n", sizeof(int));
-	printf("The size of long int is: %d\n byte(s)\n", sizeof(long int));
-	printf("The size of long long int is: %d\n byte(s)\n", sizeof(long long int));
+{	printf("The size of float is: %d byte(s)\n", sizeof(float));
+	printf("The size of char is: %d byte(s)\n", sizeof(char));
+	printf("The size of int is: %d byte(s)\n", sizeof(int));
+	printf("The size of long int is: %d byte(s)\n", sizeof(long int));
+	printf("The size of long long int is: %d byte(s)\n", sizeof(long long int));
 	return (0);
 }
