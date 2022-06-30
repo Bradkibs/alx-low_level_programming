c6946613cb0d64d766425dbda0dcf4725eafdc37
Bradleykibs <Bradleykibwana@gmail.com>
2022-06-30 14:14:49 +0000

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ *
+ */
+int main(void)
+{	char text[] = "with proper grammar, but the outcome is a piece of art,";
+	printf(text);
+	return (0);
+}
