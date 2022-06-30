8ed36bf0a315d410f17806bc2e160a4960b25666
Bradleykibs <Bradleykibwana@gmail.com>
2022-06-30 13:40:26 +0000

    adding betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void){
-	char string[]="Programming is like building a multilingual puzzle\n";
-	puts(string);
-	return(0);
+
+int main(void)
+{
+/* printing using puts*/
+	char string[] = "Programming is like building a multilingual puzzle\n";
+
+	puts (string);
+
+	return (0);
 }
