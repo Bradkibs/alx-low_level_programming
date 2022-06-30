7dbd991fab3c10f8e7ecacd717b7c800ac69701f
Bradleykibs <Bradleykibwana@gmail.com>
2022-06-30 15:38:15 +0000

    last test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  *
  */
 int main(void)
-{	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-	return (1)
+{	fprintf(stderr, "and that piece of art is useful\" - Dor\
+		a Korpar, 2015-10-19");
+	return (1);
 }
