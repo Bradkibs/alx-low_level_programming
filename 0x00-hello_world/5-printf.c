fc0e3160e438907a7496bdc2e13e447fa5976b59
Bradleykibs <Bradleykibwana@gmail.com>
2022-06-30 14:21:45 +0000

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  *
  */
 int main(void)
-{	char text[] = "with proper grammar, but the outcome is a piece of art,";
+{	char text[] = "with proper grammar, but the outcome is a piece of art,\n";
 	printf("%s", text);
 	return (0);
 }
