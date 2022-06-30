54f9e9bb708497907dbb972f380644944d57e716
Bradleykibs <Bradleykibwana@gmail.com>
2022-06-30 15:51:22 +0000

    minor change

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  *
  */
 int main(void)
-{	fprintf(stderr, "and that piece of art is useful\" - Dora" 
-		"Korpar, 2015-10-19");
+	char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+{	fprintf(stderr, "%s", txt);
 	return (1);
 }
