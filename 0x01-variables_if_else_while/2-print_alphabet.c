901ed1dc5c8825cbb25e11f00dfef4892a5239e1
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-01 18:14:12 +0000

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0 when successful
+ *
+ */
+int main(void)
+{	char ch = 'a';
+
+	while (ch <= 'z')
+	{	putchar(ch);
+		ch++;
+
+	}
+	return (0);
+}
