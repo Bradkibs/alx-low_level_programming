9104d2003d8c62b012face49e6e5160e32bd874a
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-01 21:44:43 +0000

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - returns numbers in lowercase
+ *
+ * Return:0 when successful
+ *
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{	putchar(num);
+		num++;
+	}
+	return (0);
+}
