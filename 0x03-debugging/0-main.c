05594953fee6eb4dac54c16ef22b86bdafb6ecc4
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-07 15:18:42 +0300

    first task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests the function that prints if the integer is positive or negative
+ *
+ * Return: 0 when successful
+ *
+ */
+int main(void)
+{
+	int integer;
+
+	integer = 0;
+
+	positive_or_negative(integer);
+
+	return (0);
+}
