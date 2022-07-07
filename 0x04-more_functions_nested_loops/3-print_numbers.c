622787e70277de36758e85228249f5324f9af363
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-07 19:43:50 +0300

    printing numbers 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	for (int c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
