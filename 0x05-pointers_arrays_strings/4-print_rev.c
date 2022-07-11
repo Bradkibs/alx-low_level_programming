2e67f6c8a7436a252b467d4c5e186c256750d430
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-11 19:51:39 +0300

    minor fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (*(s + i) != '\0')
+	while (*(s + i + 1) != '\0')
 	{
 		i++;
 	}
-	while (i >= 0)
+	while (i > 0)
 	{
 		_putchar(s[i]);
 		i--;
