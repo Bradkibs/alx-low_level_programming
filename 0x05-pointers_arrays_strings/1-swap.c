b8edc18be655a9ccf5a30542a1915687e0602ac7
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-11 16:45:02 +0300

    Hot fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @*a: integer containing the address of the first input
- * @*b: integer containing the address of the second input
+ * @a: integer pointer containing the address of the first input
+ * @b: integer pointer containing the address of the second input
  * Return: nothing
  */
 void swap_int(int *a, int *b)
