763743ebb94059d1d2846110df083799e01cd2e5
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-12 18:17:56 +0300

    printing integer arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: integer containing pointer of integer arrays
+ * @n: integer containing number of arrays
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		printf("\n");
+	while ( i < n)
+	{
+		if ((i + 1) == n)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
