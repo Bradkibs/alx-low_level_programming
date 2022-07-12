9c2666e1f7e22933d3b030d43272209dd9db5a5b
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-12 16:03:04 +0300

    the fifth project

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: character
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+
+	int i = 0;
+
+	char temp;
+
+	while (*(s + l) != '\0')
+	{
+		l++;
+	}
+	while ( i < l/2)
+	{
+		temp = *(s + l - i - 1);
+		*(s + l - i - 1) = *(s + i);
+		*(s + i) = temp;
+		i++;
+	}
+
+}
