a2a75b040e7cb7a2e21748237c2cc60900bb6769
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-12 18:46:33 +0300

    copying strings to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string pointed to bey the src inlcluding '\0' to the buffer
+ * @dest: pointer which is of character type
+ * @src: pointer which is the source
+ * Return: a pointer to destination of character type
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = *(dest + i);
+	return (dest);
+
+}
