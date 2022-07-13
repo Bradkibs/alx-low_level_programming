47854f1e2a85c53cce99961758464323fe23a4a3
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-13 16:59:32 +0300

    string concatination with the number of characters to be concatenated

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using n bytes from the source
+ * @dest: pointer to the destination of type character
+ * @src: pointer to the source of type character
+ * @n: number of bytes of type integer
+ * Return: a character type
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	while (*(dest + j) != '\0')
+	{
+		j++;
+	}
+	while (n > k)
+	{
+		*(dest + j + k) = *(src + k);
+		k++;
+	}
+	return (dest);
+}
