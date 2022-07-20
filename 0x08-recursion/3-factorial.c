c1b7387fe1e975bcfc5bf8025d74b318ab22188d
Bradleykibs <Bradleykibwana@gmail.com>
2022-07-20 16:11:58 +0300

    betty correction

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,13 +5,13 @@
  * n: the number to be evaluated its factorial
  * Return: an integer which is the answer of the factorial to n
  * Example:
- * 	factorial(5); --> 5 * 4 * 3 * 2 * 1 = 120
+ * factorial(5); --> 5 * 4 * 3 * 2 * 1 = 120
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	if ( n == 0 || n == 1)
+	if (n == 0 || n == 1)
 		return (1);
 	return (n * factorial(n - 1));
 }
