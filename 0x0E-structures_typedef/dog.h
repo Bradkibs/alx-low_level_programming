95c273c3cdc345abf0a547e6985cd25b81cfe16e
Bradleykibs <Bradleykibwana@gmail.com>
2022-08-02 18:26:34 +0300

    the first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains the name owner and age
+ * @name: name of the dog
+ * @owner: name of the owner of the dog
+ * @age: the age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
+
