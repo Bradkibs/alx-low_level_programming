255e91d118718e3d9a1c52bf1d21ab8b86b83ec8
Bradleykibs <Bradleykibwana@gmail.com>
2022-08-16 22:17:30 +0300

    first task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements in listint_t
+ * @h: pointer to the listint_t
+ * @return: number of items in the node of size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	for (; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
