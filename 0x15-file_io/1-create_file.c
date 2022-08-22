bdbeda76b62a3a61f19924161732d138679fbecd
Bradleykibs <Bradleykibwana@gmail.com>
2022-08-23 01:37:10 +0300

    second task modification

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,6 +29,8 @@ int create_file(const char *filename, char *text_content)
                 text_content++;
         }
 	f_writer = write (STDOUT_FILENO, text_content, len);
+	if (f_writer == -1)
+		return (-1);
 	close(fd);
 	return (1);
 
