be5cc96efb8339721be82d18b1cddf6d35b7b002
Bradleykibs <Bradleykibs@gmail.com>
2022-12-28 21:23:18 +0300

    trying out a solution

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_ht == NULL)
-	{
-		free(new_ht);
 		return (NULL);
-	}
 	new_ht->size = size;
 	if (new_ht->array == NULL)
 	{
