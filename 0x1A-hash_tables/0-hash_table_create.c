84044aab944739a07e520a05d5811f3ce8696e3c
Bradleykibs <Bradleykibs@gmail.com>
2022-12-28 21:47:35 +0300

    finding the bug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *hash_table;
 	unsigned long int i;
-	hash_table_t *new_ht;
 
-	new_ht = malloc(sizeof(hash_table_t));
-	if (new_ht == NULL)
-	{
-		free(new_ht);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	}
-	new_ht->size = size;
-	new_ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_ht->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(new_ht);
+		free(hash_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-		new_ht->array[i] = NULL;
-	return (new_ht);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
