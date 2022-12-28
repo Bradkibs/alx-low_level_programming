3b1e46121bd44e5dadd27aad5b7558bcc211d724
Bradleykibs <Bradleykibs@gmail.com>
2022-12-28 21:31:08 +0300

    fixing errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(new_ht);
 		return (NULL);
+	}
 	new_ht->size = size;
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_ht->array == NULL)
