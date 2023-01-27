3791130a250b444f7449f572b04bca194e07ba74
Bradleykibs <Bradleykibs@gmail.com>
2023-01-27 17:21:50 +0300

    fixing betty errors

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,13 +14,13 @@ int linear_search(int *array, size_t size, int value)
 	int i;
 
 	if (array == NULL)
-		return -1;
-	for (i=0; i < (int)size; i++)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, *array);
 		if (*array == value)
-			return i;
+			return (i);
 		array++;
 	}
-	return -1;
+	return (-1);
 }
